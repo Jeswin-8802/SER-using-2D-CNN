d3439cbe856d0a80032144538ed906d1c46db3ef
Jeswin Santosh <jeswin.santosh@outlook.com>
2023-02-21 11:35:02 +0000

    final commit: to be reviewed

--- scripts/model_test_v2.py
+++ scripts/model_test_v2.py
@@ -3,13 +3,14 @@ import pandas as pd
 import numpy as np
 from sklearn import preprocessing
 import plotext as plt
+import librosa
 import sys
 # sys.tracebacklimit = 0
 import os
 import re
 if os.path.abspath('../lib') not in sys.path:
     sys.path.insert(0, os.path.abspath('../lib'))
-import custom_util as util
+import util_for_2d_features as util
 import warnings
 if not sys.warnoptions:
     warnings.simplefilter("ignore")
@@ -22,16 +23,21 @@ def main():
         raise Exception("No Audio file given...")
     path_to_sound_file = sys.argv[1]
     checkFileValidity(path_to_sound_file)
-    features = util.get_features(path_to_sound_file)
-    Features = getSelectedFeatures(features) 
-    test_data = Features.values
-    np.expand_dims(test_data, axis = 2)
+    test_data = np.array([get_features(path_to_sound_file)])
+    test_data = np.expand_dims(test_data, axis = 3)
+    print(test_data.shape)
     predicted_data = model.predict(test_data)
     print(predicted_data)
-    percentages = preprocessing.minmax_scale(predicted_data, feature_range=(0, 100), axis=0, copy=True).round(0)
+    percentages = preprocessing.minmax_scale(predicted_data, feature_range=(0, 100), axis=1, copy=True).round(0)
     print(percentages)
 
     getProbableEmotion(percentages[0])
+
+
+def get_features(path_to_sound_file):
+    y, sr = librosa.load(path_to_sound_file)
+    features = util.get_features(y, sr)
+    return features
 
 
 
@@ -53,37 +59,8 @@ def checkFileValidity(path_to_sound_file):
 
 
 
-def getSelectedFeatures(features):
-    Features = pd.DataFrame(features)
-    features_to_choose = [1,2,3,5,9] # enter here
-    present_features = [i for i in range(10)]
-    to_remove = sorted([i for i in present_features if i not in features_to_choose], reverse=True)
-    track_features = [
-        ['ZCR', 1],
-        ['Chroma_stft', 12],
-        ['poly_features', 10],
-        ['MFCC', 20],
-        ['RMS', 1],
-        ['MelSpectogram', 128],
-        ['spectral centroid', 1],
-        ['spectral rolloff', 1],
-        ['spectral contrast', 7],
-        ['tonnetz', 6]
-    ]
-    column_tracker, temp = [0], 0
-    for data in track_features:
-        temp += data[1]
-        column_tracker.append(temp)
-    for num in to_remove:
-        Features = Features.drop(
-            Features.iloc[:, column_tracker[num]:column_tracker[num + 1]], axis=1)
-    
-    return Features
-
-
-
 if __name__ == '__main__':
-    model_file_location = os.path.join(os.path.abspath('..'), 'data', 'lstm_v1.pkl')
+    model_file_location = os.path.join(os.path.abspath('..'), 'data', 'lstm.pkl')
     history = joblib.load(model_file_location)
     global model
     model = history.model
